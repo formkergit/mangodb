adf17152c27b4473ca875aafabf504c404ddbefb
formkergit <formker@gmail.com>
2025-10-29 09:17:02 +0100

    Ajout des  dossiers routes et models

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+const portServer = 8002;
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/simple-api', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect('mongodb://localhost:27017/notes')
 .then(() => console.log('Connecté à mongodb'))
 .catch((err) => console.error('Erreur de connextion à mongodb !', err))
+
+app.listen(portServer, '0.0.0.0' , () => {
+    console.log(`Le serveur tourne sur port ${portServer}`);
+})

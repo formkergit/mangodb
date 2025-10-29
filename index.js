9fca7276f7fc09595c00cae156f46dc78670de69
formkergit <formker@gmail.com>
2025-10-29 10:00:37 +0100

    Ajout d'un schema dans models

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const noteRoutes = require("./routes/notes");
 
 const app = express();
 const portServer = 8002;
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/notes')
-.then(() => console.log('Connecté à mongodb'))
-.catch((err) => console.error('Erreur de connextion à mongodb !', err))
+mongoose
+  .connect("mongodb://localhost:27017/notes")
+  .then(() => console.log("Connecté à mongodb"))
+  .catch((err) => console.error("Erreur de connextion à mongodb !", err));
 
-app.listen(portServer, '0.0.0.0' , () => {
-    console.log(`Le serveur tourne sur port ${portServer}`);
-})
+app.use("/api/notes", noteRoutes);
+
+app.listen(portServer, "0.0.0.0", () => {
+  console.log(`Le serveur tourne sur port ${portServer}`);
+});
--- readme.md
+++ readme.md
@@ -1,8 +1,13 @@
 ```
-docker run -it --name mongodb  -v ${PWD}:/app -w /app -p 8002:8002 --entrypoint sh node:24-alpine
+docker network create noteapi-net
+
+docker run -it --name noteapi -v ${PWD}:/app -w /app -p 8002:8002 --network noteapi-net node:alpine /bin/sh
 
 npm init -y
 npm i express mongoose
+
+docker run -d --name mongodb -p 27017:27017 -v mongodb_data:/data/db --network noteapi-net mongo:latest
+
 ```
 
 [https://www.mongodb.com/](https://www.mongodb.com/)

b279775577a0fd385745ca707bf4b1df32e4d84c
formkergit <formker@gmail.com>
2025-10-31 14:19:30 +0100

    Ajout de faker.js

---
+++ models/faker.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { faker } = require("@faker-js/faker");
+
+const noteSchema = new mongoose.Schema({
+  titre: { type: String, required: true },
+  contenu: { type: String, required: true },
+});
+
+const createNote = () => {
+  titre: faker.lorem.lines(2);
+  contenu: faker.lorem.paragraphs(5);
+};
+
+const generateNotes = (nombre) => Array.from({ length: nombre }, createNote);
+
+async function remplirBasedeDonnee(nombre = 10) {
+  const Note = mongoose.model("Note", noteSchema);
+  try {
+    const notes = generateNotes(nombre);
+    await Note.insertMany(notes);
+    return true;
+  } catch (error) {
+    console.error("Erreur pour remplir la base de donée : ", error);
+    throw error;
+  }
+}
+
+remplirBasedeDonnee();
+
+module.exports = mongoose.model('Note', noteSchema);
